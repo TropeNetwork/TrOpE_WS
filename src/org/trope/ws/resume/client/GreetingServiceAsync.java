5d05c556c46aa8f3fa86ed14aef5c05075fefdc8
Carlos Delfino Carvalho Pinheiro <consultoria@carlosdelfino.eti.br>
2011-03-24 00:40:47 -0300

    Base to start a new projet, Hello Word on google apps

---
+++ src/org/trope/ws/resume/client/GreetingServiceAsync.java
@@ -0,0 +1,11 @@
+package org.trope.ws.resume.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+	void greetServer(String input, AsyncCallback<String> callback)
+			throws IllegalArgumentException;
+}
